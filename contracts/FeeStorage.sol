fb5b7edf5c7ce4b216640c8c4570fc24e33ffed6
Pylyp Radionov <p.radionov@4irelabs.com>
2021-05-06 18:33:48 +0300

    feat: licence (#7)
    * added licence fil
    * updated package.json
    * updated README
    * added licence to contracts

--- contracts/FeeStorage.sol
+++ contracts/FeeStorage.sol
@@ -1,4 +1,5 @@
-pragma solidity >=0.7.0 <0.9.0;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
--- contracts/ManualTrade.sol
+++ contracts/ManualTrade.sol
@@ -1,4 +1,5 @@
-pragma solidity >=0.7.0 <0.9.0;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
