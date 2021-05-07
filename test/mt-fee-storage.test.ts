edca4cb0b6d9148fb000310122fff7b4c8c8f81e
a-sandyuk <artem.s@4irelabs.com>
2021-05-07 20:35:52 +0300

    feat: add test

--- test/mt-fee-storage.test.ts
+++ test/mt-fee-storage.test.ts
@@ -3,7 +3,11 @@ import { ethers } from 'hardhat';
 import { expect } from 'chai';
 import { providers, utils } from 'ethers';
 import { ContractReceipt, ContractTransaction } from "ethers"
+<<<<<<< HEAD
 import { FeeStorage, ERC20Mock} from '../typechain';
+=======
+import { FeeStorage, Erc20Mock } from '../typechain';
+>>>>>>> ab5b742... feat: add test
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { deployMockContract, MockContract } from '@ethereum-waffle/mock-contract';
 
@@ -17,14 +21,18 @@ describe('Fs-storage :: deploy test suite', () => {
     let uniswapMock: MockContract;
     let tx: ContractTransaction
     let txr: ContractReceipt
-    
+
     before('init signers', async () => {
         [owner, user, token1, token2, weth] = await ethers.getSigners();
     });
-    
-    async function getToken(token:SignerWithAddress) {
+
+    async function getToken(token: SignerWithAddress) {
         const Erc20Mock = await ethers.getContractFactory("ERC20Mock")
+<<<<<<< HEAD
         const  t = await Erc20Mock.connect(token).deploy("MockToken", "MT") as ERC20Mock
+=======
+        const t = await Erc20Mock.connect(token).deploy("MockToken", "MT") as Erc20Mock
+>>>>>>> ab5b742... feat: add test
         await t.deployed()
         return t
     }
@@ -34,16 +42,16 @@ describe('Fs-storage :: deploy test suite', () => {
         fs = await FeeStorage.connect(owner).deploy() as FeeStorage;
         await fs.deployed();
         fsDeployTxr = await fs.deployTransaction.wait();
-        
+
         await owner.sendTransaction({ to: fs.address, value: utils.parseEther('100') })
     });
-    
+
     before("deploy alphrToken mock and mint", async () => {
         const ERC20Mock = await ethers.getContractFactory("ERC20Mock")
         alphrToken = await ERC20Mock.connect(owner).deploy("MockToken", "MT") as ERC20Mock
         await alphrToken.deployed()
         await alphrToken.connect(owner).mint()
-        
+
         alphrToken.connect(owner).transfer(fs.address, utils.parseEther('2'))
     })
 
@@ -54,7 +62,11 @@ describe('Fs-storage :: deploy test suite', () => {
     });
 
     describe('check token number in list', () => {
+<<<<<<< HEAD
         let tokenA : ERC20Mock
+=======
+        let tokenA: Erc20Mock
+>>>>>>> ab5b742... feat: add test
         before('deploy and mint token', async () => {
             tokenA = await getToken(token1)
         });
@@ -63,16 +75,16 @@ describe('Fs-storage :: deploy test suite', () => {
             fs.addTokenToBalanceList(tokenA.address)
             expect(await fs.getNumberOfTokens()).to.be.eq('1')
         });
-        
+
         it('add token', async () => {
             await expect(fs.connect(user).addTokenToBalanceList(tokenA.address)).to.be.revertedWith('revert Ownable: caller is not the owner')
         });
-        
+
         it('check balance by non-owner', async () => {
             await expect(fs.connect(user).getNumberOfTokens()).to.be.revertedWith('revert Ownable: caller is not the owner')
         });
     });
-    
+
     describe('swap tokens and burn', () => {
         let tokenA, tokenB: ERC20Mock
         before('deploy and mint tokens', async () => {
@@ -81,12 +93,12 @@ describe('Fs-storage :: deploy test suite', () => {
 
             tokenB = await getToken(token2)
             tokenB.mint()
-            
+
             tokenA.transfer(fs.address, utils.parseEther('2'))
             tokenB.transfer(fs.address, utils.parseEther('5'))
         });
-        
-        before('set uniswap router addres and alphr token ', async () =>  {
+
+        before('set uniswap router addres and alphr token ', async () => {
             uniswapMock = await deployMockContract(owner, UNI.abi);
             uniswapMock.mock.getAmountsOut.returns([0, utils.parseEther('2')]);
             uniswapMock.mock.swapExactTokensForTokens.returns([])
@@ -94,21 +106,64 @@ describe('Fs-storage :: deploy test suite', () => {
             await fs.setAlphrTokenAddress(alphrToken.address);
         })
 
-        
+
         it.skip('mt-swapTokensForAlphrAndBurn', async () => {
             await fs.addTokenToBalanceList(tokenA.address);
             let path1 = ['0x2e983a1ba5e8b38aaaec4b440b9ddcfbf72e15d1', '0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0'];
             console.log(path1);
             let amounts = [0, utils.parseEther('2')]
             uniswapMock.mock.getAmountsOut.withArgs(utils.parseEther('10'), path1).returns(amounts);
-            
+
             tx = await fs.swapTokensForAlphrAndBurn()
             txr = await tx.wait()
-            
+
             const expectedEventName = fs.interface.events["Transfer(address,address, uint256)"].name
             const actualEventName = txr.events[0].event
             expect(actualEventName).to.be.equal(expectedEventName);
         });
     });
-    
+
+    describe('swap to ETH and send', () => {
+        let tokenA, tokenB: Erc20Mock
+        before('deploy and mint tokens', async () => {
+            tokenA = await getToken(token1)
+            tokenA.mint()
+
+            tokenB = await getToken(token2)
+            tokenB.mint()
+
+            await tokenA.transfer(fs.address, utils.parseEther('2'))
+            await tokenB.transfer(fs.address, utils.parseEther('5'))
+        });
+
+        before('set uniswap router address', async () => {
+            uniswapMock = await deployMockContract(owner, UNI.abi);
+            uniswapMock.mock.WETH.returns(utils.getAddress('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'));
+            uniswapMock.mock.getAmountsOut.returns([0, utils.parseEther('2')]);
+            uniswapMock.mock.swapExactTokensForETH.returns([0, utils.parseEther('2')]);
+            uniswapMock.mock.swapExactTokensForTokens.returns([]);
+            await fs.setUniswapRouterAddress(uniswapMock.address);
+            await fs.setAlphrTokenAddress(alphrToken.address);
+        })
+
+        it('mt-swapToETHAndSend', async () => {
+            await fs.addTokenToBalanceList(tokenA.address);
+            await fs.addTokenToBalanceList(tokenB.address);
+
+            let prov = providers.getDefaultProvider()
+            let addressTo = '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266';
+
+            console.log("Old balance: ", ethers.utils.formatEther(await prov.getBalance(addressTo)))
+            tx = await fs.swapToETHAndSend(utils.getAddress(addressTo));
+            txr = await tx.wait();
+            console.log("New balance: ", ethers.utils.formatEther(await prov.getBalance(addressTo)))
+
+            console.log("Send events emitted: ")
+            txr.events.forEach(e => {
+                if (e.eventSignature == "SendETH(uint256,address,bytes)")
+                    console.log(e.args)
+            })
+        });
+    });
 });
+
