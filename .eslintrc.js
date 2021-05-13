0bbf4b513fd0497f0b6b4b51a29815ab88f3a419
a-sandyuk <artem.s@4irelabs.com>
2021-05-13 13:01:38 +0300

    feat: add ts linter configs

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    env: {
+        browser: true,
+        jest: true,
+    },
+    extends: [
+        'prettier',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+        'plugin:jest/recommended',
+    ],
+    plugins: ['prettier', '@typescript-eslint', 'jest'],
+    settings: {
+        'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+        'import/parsers': {
+            '@typescript-eslint/parser': ['.js', '.jsx', '.ts', '.tsx'],
+        },
+        'import/resolver': {
+            node: {
+                extensions: ['.js', '.jsx', '.ts', '.tsx'],
+            },
+        },
+    },
+    rules: {
+        'prettier/prettier': [
+            'error',
+            { singleQuote: true, endOfLine: 'auto' },
+        ],
+        'import/extensions': [0],
+        'prefer-destructuring': ['error', { object: true, array: false }],
+        'no-negated-condition': 'error',
+        'no-use-before-define': 'off',
+        '@typescript-eslint/no-use-before-define': ['error'],
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': ['error'],
+        'import/prefer-default-export': 'off',
+        'no-param-reassign': 'off',
+        'no-shadow': 'off',
+        'no-implicit-coercion': [
+            'error',
+            {
+                boolean: true,
+                number: true,
+                string: true,
+                allow: [],
+            },
+        ],
+    },
+    ignorePatterns: ['/dist/*', '/coverage/*', '/lib/*'],
+};
