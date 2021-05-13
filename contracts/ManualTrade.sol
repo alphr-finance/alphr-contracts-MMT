9a51940e3a1b1e5fa823d939bc93446f0e8121d4
a-sandyuk <artem.s@4irelabs.com>
2021-05-13 16:57:27 +0300

    fix: linter errors

--- contracts/ManualTrade.sol
+++ contracts/ManualTrade.sol
@@ -8,137 +8,135 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./FeeStorage.sol";
 
 contract ManualTrade is Ownable {
-    using SafeMath for uint256;
+  using SafeMath for uint256;
 
-    uint256 private feeQuota;
-    uint256 private feeQuotaDecimals;
+  uint256 private feeQuota;
+  uint256 private feeQuotaDecimals;
 
-    FeeStorage private feeStorage;
-    IUniswapV2Router02 private uniswap;
+  FeeStorage private feeStorage;
+  IUniswapV2Router02 private uniswap;
 
-    constructor(
-        address payable _feeStorage,
-        uint256 _feeQuota,
-        uint256 _feeQuotaDecimals,
-        address _uniswap
-    ) {
-        feeQuota = _feeQuota;
-        feeQuotaDecimals = _feeQuotaDecimals;
-        feeStorage = FeeStorage(_feeStorage);
-        uniswap = IUniswapV2Router02(_uniswap);
-    }
+  constructor(
+    address payable _feeStorage,
+    uint256 _feeQuota,
+    uint256 _feeQuotaDecimals,
+    address _uniswap
+  ) {
+    feeQuota = _feeQuota;
+    feeQuotaDecimals = _feeQuotaDecimals;
+    feeStorage = FeeStorage(_feeStorage);
+    uniswap = IUniswapV2Router02(_uniswap);
+  }
 
-    function swapExactTokensForTokens(
-        uint256 amountIn,
-        uint256 amountOutMin,
-        address[] calldata path
-    ) public {
-        address tokenIn = path[0];
+  function swapExactTokensForTokens(
+    uint256 amountIn,
+    uint256 amountOutMin,
+    address[] calldata path
+  ) public {
+    address tokenIn = path[0];
 
-        // step 0: transfer tokenIn from user to contracts balance
-        require(
-            ERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn),
-            "low allowance for contract"
-        );
+    // step 0: transfer tokenIn from user to contracts balance
+    require(
+      ERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn),
+      "low allowance for contract"
+    );
 
-        // step 1: calculate fee amount
-        uint256 tokenInDecimals = ERC20(tokenIn).decimals();
-        uint256 feeAmount =
-            calculateFee(feeQuota, feeQuotaDecimals, tokenInDecimals, amountIn);
+    // step 1: calculate fee amount
+    uint256 tokenInDecimals = ERC20(tokenIn).decimals();
+    uint256 feeAmount =
+      calculateFee(feeQuota, feeQuotaDecimals, tokenInDecimals, amountIn);
 
-        // step 2: sub fee from amountIn
-        uint256 swapAmountIn = amountIn.sub(feeAmount);
+    // step 2: sub fee from amountIn
+    uint256 swapAmountIn = amountIn.sub(feeAmount);
 
-        // step 3: approve uniswap router
-        require(IERC20(tokenIn).approve(address(uniswap), swapAmountIn));
+    // step 3: approve uniswap router
+    require(IERC20(tokenIn).approve(address(uniswap), swapAmountIn));
 
-        // step 4: send token to feeStorage
-        ERC20(tokenIn).transfer(address(feeStorage), feeAmount);
+    // step 4: send token to feeStorage
+    ERC20(tokenIn).transfer(address(feeStorage), feeAmount);
 
-        // step 5: execute swap
-        uniswap.swapExactTokensForTokens(
-            swapAmountIn,
-            amountOutMin,
-            path,
-            msg.sender,
-            block.timestamp
-        );
-    }
+    // step 5: execute swap
+    uniswap.swapExactTokensForTokens(
+      swapAmountIn,
+      amountOutMin,
+      path,
+      msg.sender,
+      block.timestamp
+    );
+  }
 
-    function swapExactTokensForETH(
-        uint256 amountIn,
-        uint256 amountOutMin,
-        address[] calldata path
-    ) public {
-        address tokenIn = path[0];
+  function swapExactTokensForETH(
+    uint256 amountIn,
+    uint256 amountOutMin,
+    address[] calldata path
+  ) public {
+    address tokenIn = path[0];
 
-        // step 0: transfer tokenIn from user to cotnracts balance
-        require(
-            ERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn),
-            "low allowance for contract"
-        );
+    // step 0: transfer tokenIn from user to cotnracts balance
+    require(
+      ERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn),
+      "low allowance for contract"
+    );
 
-        // step 1: calculate fee amount
-        uint256 tokenInDecimals = ERC20(tokenIn).decimals();
-        uint256 feeAmount =
-            calculateFee(feeQuota, feeQuotaDecimals, tokenInDecimals, amountIn);
+    // step 1: calculate fee amount
+    uint256 tokenInDecimals = ERC20(tokenIn).decimals();
+    uint256 feeAmount =
+      calculateFee(feeQuota, feeQuotaDecimals, tokenInDecimals, amountIn);
 
-        // step 2: sub fee from amountIn
-        uint256 swapAmountIn = amountIn.sub(feeAmount);
-        // step 3: approve uniswap router
-        require(IERC20(tokenIn).approve(address(uniswap), swapAmountIn));
+    // step 2: sub fee from amountIn
+    uint256 swapAmountIn = amountIn.sub(feeAmount);
+    // step 3: approve uniswap router
+    require(IERC20(tokenIn).approve(address(uniswap), swapAmountIn));
 
-        // step 4: send token to feeStorage
-        ERC20(tokenIn).transfer(address(feeStorage), feeAmount);
+    // step 4: send token to feeStorage
+    ERC20(tokenIn).transfer(address(feeStorage), feeAmount);
 
-        // step 5: execute swap
-        uniswap.swapExactTokensForETH(
-            swapAmountIn,
-            amountOutMin,
-            path,
-            msg.sender,
-            block.timestamp
-        );
-    }
+    // step 5: execute swap
+    uniswap.swapExactTokensForETH(
+      swapAmountIn,
+      amountOutMin,
+      path,
+      msg.sender,
+      block.timestamp
+    );
+  }
 
-    function swapExactETHForTokens(
-        uint256 amountOutMin,
-        address[] calldata path
-    ) public payable {
-        // step 0: calculate fee amount
-        uint256 feeAmount =
-            calculateFee(feeQuota, feeQuotaDecimals, 18, msg.value);
+  function swapExactETHForTokens(uint256 amountOutMin, address[] calldata path)
+    public
+    payable
+  {
+    // step 0: calculate fee amount
+    uint256 feeAmount = calculateFee(feeQuota, feeQuotaDecimals, 18, msg.value);
 
-        // step 1: sub fee from amountIn
-        uint256 swapAmountIn = uint256(msg.value).sub(feeAmount);
+    // step 1: sub fee from amountIn
+    uint256 swapAmountIn = uint256(msg.value).sub(feeAmount);
 
-        // step 2: execute swap
-        uniswap.swapExactETHForTokens{value: swapAmountIn}(
-            amountOutMin,
-            path,
-            msg.sender,
-            block.timestamp
-        );
-    }
+    // step 2: execute swap
+    uniswap.swapExactETHForTokens{value: swapAmountIn}(
+      amountOutMin,
+      path,
+      msg.sender,
+      block.timestamp
+    );
+  }
 
-    function setFeeQuota(uint256 _feeQuota, uint256 _feeQuotaDecimals)
-        public
-        onlyOwner
-    {
-        feeQuota = _feeQuota;
-        feeQuotaDecimals = _feeQuotaDecimals;
-    }
+  function setFeeQuota(uint256 _feeQuota, uint256 _feeQuotaDecimals)
+    public
+    onlyOwner
+  {
+    feeQuota = _feeQuota;
+    feeQuotaDecimals = _feeQuotaDecimals;
+  }
 
-    function calculateFee(
-        uint256 _feeQuota,
-        uint256 _feeQuotaDecimals,
-        uint256 _tokenDecimals,
-        uint256 _amount
-    ) public pure returns (uint256) {
-        uint256 feeQuoteNormalized =
-            _feeQuota.mul(10**_tokenDecimals).div(_feeQuotaDecimals);
-        uint256 feeAmount =
-            _amount.mul(feeQuoteNormalized).div(10**_tokenDecimals);
-        return feeAmount;
-    }
+  function calculateFee(
+    uint256 _feeQuota,
+    uint256 _feeQuotaDecimals,
+    uint256 _tokenDecimals,
+    uint256 _amount
+  ) public pure returns (uint256) {
+    uint256 feeQuoteNormalized =
+      _feeQuota.mul(10**_tokenDecimals).div(_feeQuotaDecimals);
+    uint256 feeAmount = _amount.mul(feeQuoteNormalized).div(10**_tokenDecimals);
+    return feeAmount;
+  }
 }
--- test/fee-calculate.test.ts
+++ test/fee-calculate.test.ts
@@ -1,15 +1,17 @@
 // @ts-ignore
 import { ethers, network } from 'hardhat';
-import { providers, utils } from 'ethers';
+import { utils } from 'ethers';
 import { FeeStorage, ManualTrade, ERC20Mock } from '../typechain';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { expect } from 'chai';
-import { deployMockContract, MockContract } from '@ethereum-waffle/mock-contract';
+import {
+  deployMockContract,
+  MockContract,
+} from '@ethereum-waffle/mock-contract';
 
-const UNI = require("../artifacts/@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol/IUniswapV2Router02.json")
+const UNI = require('../artifacts/@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol/IUniswapV2Router02.json');
 
 describe('ManualTrade :: fee calculations test', () => {
-
   let deployer, user: SignerWithAddress;
   const feeQuota = 20;
   const feeQuotaDecimals = 10000;
@@ -21,214 +23,304 @@ describe('ManualTrade :: fee calculations test', () => {
   });
 
   let fs: FeeStorage;
-  let fsDeployTxr: providers.TransactionReceipt;
-  let uniswapMock: MockContract
+  let uniswapMock: MockContract;
 
   before('deploy fee storage', async () => {
     const FeeStorage = await ethers.getContractFactory('FeeStorage');
-    fs = await FeeStorage.connect(deployer).deploy() as FeeStorage;
+    fs = (await FeeStorage.connect(deployer).deploy()) as FeeStorage;
     await fs.deployed();
-    fsDeployTxr = await fs.deployTransaction.wait();
+    await fs.deployTransaction.wait();
   });
 
   let mt: ManualTrade;
-  let mtDeployTxr: providers.TransactionReceipt;
 
   before('deploy manual trade contract', async () => {
     const ManualTrade = await ethers.getContractFactory('ManualTrade');
     uniswapMock = await deployMockContract(user, UNI.abi);
-    mt = await ManualTrade.connect(deployer).deploy(
+    mt = (await ManualTrade.connect(deployer).deploy(
       fs.address,
-      feeQuota, feeQuotaDecimals,
+      feeQuota,
+      feeQuotaDecimals,
       uniswapMock.address
-    ) as ManualTrade;
+    )) as ManualTrade;
     await mt.deployed();
-    mtDeployTxr = await mt.deployTransaction.wait();
+    await mt.deployTransaction.wait();
   });
 
-  describe('WETH fee calculation', async () => {
-
+  describe('WETH fee calculation', () => {
     it('correct fee for 1 WETH', async () => {
       const amount = ethers.utils.parseUnits('1', WETHDecimals);
       const actualFeeAmount = await mt.calculateFee(
-        feeQuota, feeQuotaDecimals,
-        WETHDecimals, amount);
+        feeQuota,
+        feeQuotaDecimals,
+        WETHDecimals,
+        amount
+      );
       const expectedFeeAmount = ethers.utils.parseUnits('0.002', WETHDecimals);
       const actual = ethers.utils.formatUnits(actualFeeAmount, WETHDecimals);
-      const expected = ethers.utils.formatUnits(expectedFeeAmount, WETHDecimals);
+      const expected = ethers.utils.formatUnits(
+        expectedFeeAmount,
+        WETHDecimals
+      );
       expect(actual).to.be.eq(expected);
     });
 
     it('correct fee for 100 WETH', async () => {
       const amount = ethers.utils.parseUnits('100', WETHDecimals);
       const actualFeeAmount = await mt.calculateFee(
-        feeQuota, feeQuotaDecimals,
-        WETHDecimals, amount);
+        feeQuota,
+        feeQuotaDecimals,
+        WETHDecimals,
+        amount
+      );
       const expectedFeeAmount = ethers.utils.parseUnits('0.2', WETHDecimals);
       const actual = ethers.utils.formatUnits(actualFeeAmount, WETHDecimals);
-      const expected = ethers.utils.formatUnits(expectedFeeAmount, WETHDecimals);
+      const expected = ethers.utils.formatUnits(
+        expectedFeeAmount,
+        WETHDecimals
+      );
       expect(actual).to.be.eq(expected);
     });
 
     it('correct fee for 34523 WETH', async () => {
       const amount = ethers.utils.parseUnits('34523', WETHDecimals);
       const actualFeeAmount = await mt.calculateFee(
-        feeQuota, feeQuotaDecimals,
-        WETHDecimals, amount);
+        feeQuota,
+        feeQuotaDecimals,
+        WETHDecimals,
+        amount
+      );
       const expectedFeeAmount = ethers.utils.parseUnits('69.046', WETHDecimals);
       const actual = ethers.utils.formatUnits(actualFeeAmount, WETHDecimals);
-      const expected = ethers.utils.formatUnits(expectedFeeAmount, WETHDecimals);
+      const expected = ethers.utils.formatUnits(
+        expectedFeeAmount,
+        WETHDecimals
+      );
       expect(actual).to.be.eq(expected);
     });
-
 
     // todo unskip after `contracts/ManualTrade/ManualTrade.sol:71` fixed
     it('correct fee for 2.1 WETH', async () => {
       const amount = ethers.utils.parseUnits('2.1', WETHDecimals);
       const actualFeeAmount = await mt.calculateFee(
-        feeQuota, feeQuotaDecimals,
-        WETHDecimals, amount);
+        feeQuota,
+        feeQuotaDecimals,
+        WETHDecimals,
+        amount
+      );
       const expectedFeeAmount = ethers.utils.parseUnits('0.0042', WETHDecimals);
       const actual = ethers.utils.formatUnits(actualFeeAmount, WETHDecimals);
-      const expected = ethers.utils.formatUnits(expectedFeeAmount, WETHDecimals);
+      const expected = ethers.utils.formatUnits(
+        expectedFeeAmount,
+        WETHDecimals
+      );
       expect(actual).to.be.eq(expected);
     });
 
     it('correct fee for 2.113125123 WETH', async () => {
       const amount = ethers.utils.parseUnits('2.113125123', WETHDecimals);
       const actualFeeAmount = await mt.calculateFee(
-        feeQuota, feeQuotaDecimals,
-        WETHDecimals, amount);
-      const expectedFeeAmount = ethers.utils.parseUnits('0.004226250246', WETHDecimals);
+        feeQuota,
+        feeQuotaDecimals,
+        WETHDecimals,
+        amount
+      );
+      const expectedFeeAmount = ethers.utils.parseUnits(
+        '0.004226250246',
+        WETHDecimals
+      );
       const actual = ethers.utils.formatUnits(actualFeeAmount, WETHDecimals);
-      const expected = ethers.utils.formatUnits(expectedFeeAmount, WETHDecimals);
+      const expected = ethers.utils.formatUnits(
+        expectedFeeAmount,
+        WETHDecimals
+      );
       expect(actual).to.be.eq(expected);
     });
 
     it('correct fee for 1234567890.12345 WETH', async () => {
       const amount = ethers.utils.parseUnits('1234567890.12345', WETHDecimals);
       const actualFeeAmount = await mt.calculateFee(
-        feeQuota, feeQuotaDecimals,
-        WETHDecimals, amount);
-      const expectedFeeAmount = ethers.utils.parseUnits('2469135.7802469', WETHDecimals);
+        feeQuota,
+        feeQuotaDecimals,
+        WETHDecimals,
+        amount
+      );
+      const expectedFeeAmount = ethers.utils.parseUnits(
+        '2469135.7802469',
+        WETHDecimals
+      );
       const actual = ethers.utils.formatUnits(actualFeeAmount, WETHDecimals);
-      const expected = ethers.utils.formatUnits(expectedFeeAmount, WETHDecimals);
+      const expected = ethers.utils.formatUnits(
+        expectedFeeAmount,
+        WETHDecimals
+      );
       expect(actual).to.be.eq(expected);
     });
   });
 
-  describe('WBTC fee calculation', async () => {
+  describe('WBTC fee calculation', () => {
     it('correct fee for 1 WBTC', async () => {
       const amount = ethers.utils.parseUnits('1', WBTCDecimals);
       const actualFeeAmount = await mt.calculateFee(
-        feeQuota, feeQuotaDecimals,
-        WBTCDecimals, amount);
+        feeQuota,
+        feeQuotaDecimals,
+        WBTCDecimals,
+        amount
+      );
       const expectedFeeAmount = ethers.utils.parseUnits('0.002', WBTCDecimals);
       const actual = ethers.utils.formatUnits(actualFeeAmount, WBTCDecimals);
-      const expected = ethers.utils.formatUnits(expectedFeeAmount, WBTCDecimals);
+      const expected = ethers.utils.formatUnits(
+        expectedFeeAmount,
+        WBTCDecimals
+      );
       expect(actual).to.be.eq(expected);
     });
 
     it('correct fee for 100 WBTC', async () => {
       const amount = ethers.utils.parseUnits('100', WBTCDecimals);
       const actualFeeAmount = await mt.calculateFee(
-        feeQuota, feeQuotaDecimals,
-        WBTCDecimals, amount);
+        feeQuota,
+        feeQuotaDecimals,
+        WBTCDecimals,
+        amount
+      );
       const expectedFeeAmount = ethers.utils.parseUnits('0.2', WBTCDecimals);
       const actual = ethers.utils.formatUnits(actualFeeAmount, WBTCDecimals);
-      const expected = ethers.utils.formatUnits(expectedFeeAmount, WBTCDecimals);
+      const expected = ethers.utils.formatUnits(
+        expectedFeeAmount,
+        WBTCDecimals
+      );
       expect(actual).to.be.eq(expected);
     });
 
     it('correct fee for 34523 WBTC', async () => {
       const amount = ethers.utils.parseUnits('34523', WBTCDecimals);
       const actualFeeAmount = await mt.calculateFee(
-        feeQuota, feeQuotaDecimals,
-        WBTCDecimals, amount);
+        feeQuota,
+        feeQuotaDecimals,
+        WBTCDecimals,
+        amount
+      );
       const expectedFeeAmount = ethers.utils.parseUnits('69.046', WBTCDecimals);
       const actual = ethers.utils.formatUnits(actualFeeAmount, WBTCDecimals);
-      const expected = ethers.utils.formatUnits(expectedFeeAmount, WBTCDecimals);
+      const expected = ethers.utils.formatUnits(
+        expectedFeeAmount,
+        WBTCDecimals
+      );
       expect(actual).to.be.eq(expected);
     });
 
     it('correct fee for 2.1 WBTC', async () => {
       const amount = ethers.utils.parseUnits('2.1', WBTCDecimals);
       const actualFeeAmount = await mt.calculateFee(
-        feeQuota, feeQuotaDecimals,
-        WBTCDecimals, amount);
+        feeQuota,
+        feeQuotaDecimals,
+        WBTCDecimals,
+        amount
+      );
       const expectedFeeAmount = ethers.utils.parseUnits('0.0042', WBTCDecimals);
       const actual = ethers.utils.formatUnits(actualFeeAmount, WBTCDecimals);
-      const expected = ethers.utils.formatUnits(expectedFeeAmount, WBTCDecimals);
+      const expected = ethers.utils.formatUnits(
+        expectedFeeAmount,
+        WBTCDecimals
+      );
       expect(actual).to.be.eq(expected);
     });
   });
 
-  describe('mt-test swap ERC20 for ETH token', async () => {
-    let token: ERC20Mock
+  describe('mt-test swap ERC20 for ETH token', () => {
+    let token: ERC20Mock;
 
-    before("deploy ERC20 mock and mint", async () => {
-      const Erc20Mock = await ethers.getContractFactory("ERC20Mock")
-      token = await Erc20Mock.connect(deployer).deploy("MockToken", "MT") as ERC20Mock
-      await token.deployed()
-      await token.connect(user).mint()
-    })
+    before('deploy ERC20 mock and mint', async () => {
+      const Erc20Mock = await ethers.getContractFactory('ERC20Mock');
+      token = (await Erc20Mock.connect(deployer).deploy(
+        'MockToken',
+        'MT'
+      )) as ERC20Mock;
+      await token.deployed();
+      await token.connect(user).mint();
+    });
 
     it('mt-test swapExactTokensForETH', async () => {
       const actualFeeAmount = await mt.calculateFee(
-        feeQuota, feeQuotaDecimals,
-        WETHDecimals, utils.parseEther('2'));
+        feeQuota,
+        feeQuotaDecimals,
+        WETHDecimals,
+        utils.parseEther('2')
+      );
 
-      uniswapMock.mock.swapExactTokensForETH.returns([])
-      await token.connect(user).approve(mt.address, utils.parseEther('2'))
-      await mt.connect(user).swapExactTokensForETH(
-        utils.parseEther('2'),
-        utils.parseEther('4'),
-        [token.address, "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6"])
+      uniswapMock.mock.swapExactTokensForETH.returns([]);
+      await token.connect(user).approve(mt.address, utils.parseEther('2'));
+      await mt
+        .connect(user)
+        .swapExactTokensForETH(utils.parseEther('2'), utils.parseEther('4'), [
+          token.address,
+          '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6',
+        ]);
 
-      expect(await (await token.balanceOf(fs.address)).toString()).to.be.eq(actualFeeAmount.toString())
-    })
+      expect(await (await token.balanceOf(fs.address)).toString()).to.be.eq(
+        actualFeeAmount.toString()
+      );
+    });
   });
 
-  describe('mt-test swap ERC20 for tokens', async () => {
-    let token, token1: ERC20Mock
+  describe('mt-test swap ERC20 for tokens', () => {
+    let token, token1: ERC20Mock;
 
-    before("deploy ERC20 mock and mint", async () => {
-      const Erc20Mock = await ethers.getContractFactory("ERC20Mock")
-      token = await Erc20Mock.connect(deployer).deploy("MockToken", "MT") as ERC20Mock
-      await token.deployed()
-      await token.connect(user).mint()
+    before('deploy ERC20 mock and mint', async () => {
+      const Erc20Mock = await ethers.getContractFactory('ERC20Mock');
+      token = (await Erc20Mock.connect(deployer).deploy(
+        'MockToken',
+        'MT'
+      )) as ERC20Mock;
+      await token.deployed();
+      await token.connect(user).mint();
 
-      const Erc20Mock1 = await ethers.getContractFactory("ERC20Mock")
-      token1 = await Erc20Mock1.connect(deployer).deploy("MockToken", "MT") as ERC20Mock
-      await token1.deployed()
-      await token1.connect(user).mint()
-    })
+      const Erc20Mock1 = await ethers.getContractFactory('ERC20Mock');
+      token1 = (await Erc20Mock1.connect(deployer).deploy(
+        'MockToken',
+        'MT'
+      )) as ERC20Mock;
+      await token1.deployed();
+      await token1.connect(user).mint();
+    });
 
     it('mt-test swapExactTokensForETH', async () => {
       const actualFeeAmount = await mt.calculateFee(
-        feeQuota, feeQuotaDecimals,
-        WETHDecimals, utils.parseEther('2'));
+        feeQuota,
+        feeQuotaDecimals,
+        WETHDecimals,
+        utils.parseEther('2')
+      );
 
-      uniswapMock.mock.swapExactTokensForTokens.returns([])
-      await token.connect(user).approve(mt.address, utils.parseEther('2'))
-      await mt.connect(user).swapExactTokensForTokens(
-        utils.parseEther('2'),
-        utils.parseEther('4'),
-        [token.address, token1.address])
+      uniswapMock.mock.swapExactTokensForTokens.returns([]);
+      await token.connect(user).approve(mt.address, utils.parseEther('2'));
+      await mt
+        .connect(user)
+        .swapExactTokensForTokens(
+          utils.parseEther('2'),
+          utils.parseEther('4'),
+          [token.address, token1.address]
+        );
 
-      expect(await (await token.balanceOf(fs.address)).toString()).to.be.eq(actualFeeAmount.toString())
-    })
+      expect(await (await token.balanceOf(fs.address)).toString()).to.be.eq(
+        actualFeeAmount.toString()
+      );
+    });
   });
 
   after('reset node fork', async () => {
     await network.provider.request({
-      method: "hardhat_reset",
-      params: [{
-        forking: {
-          jsonRpcUrl: "https://eth-mainnet.alchemyapi.io/v2/iHddcEw1BVe03s2BXSQx_r_BTDE-jDxB",
-          blockNumber: 12419631
-        }
-      }]
+      method: 'hardhat_reset',
+      params: [
+        {
+          forking: {
+            jsonRpcUrl:
+              'https://eth-mainnet.alchemyapi.io/v2/iHddcEw1BVe03s2BXSQx_r_BTDE-jDxB',
+            blockNumber: 12419631,
+          },
+        },
+      ],
     });
   });
 });
