1f31b33a2ad5d1c5330677d49cee1cf0a92b56b3
Pylyp Radionov <p.radionov@4irelabs.com>
2021-06-01 19:08:51 +0300

    fix: imports clean up

--- contracts/FeeStorage.sol
+++ contracts/FeeStorage.sol
@@ -2,15 +2,13 @@
 pragma solidity 0.7.6;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/EnumerableSet.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "./IWETH9.sol";
 
-contract FeeStorage is Ownable, AccessControl {
+contract FeeStorage is Ownable {
   using SafeERC20 for IERC20;
   using SafeMath for uint256;
 
