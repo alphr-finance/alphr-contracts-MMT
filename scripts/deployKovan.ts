060829e6c91e74dd87fbea3ef2c937743dcd0ae2
cryptojp <smart.solidity@gmail.com>
2021-08-13 15:02:17 -0400

    fix: improve gas(external functions)

--- scripts/deployKovan.ts
+++ scripts/deployKovan.ts
@@ -1,26 +1,27 @@
 // @ts-ignore
-import { ethers } from 'hardhat';
-import { UNISWAP_ROUTER_V2 } from '../constants/uniswap';
-import { KOVAN_ALPHR_TOKEN } from '../constants/tokens';
-import { KOVAN_ALPHR_VAULT } from '../constants/alphr';
+import { ethers } from "hardhat";
+import { UNISWAP_ROUTER_V2 } from "../constants/uniswap";
+import { KOVAN_ALPHR_TOKEN } from "../constants/tokens";
+import { KOVAN_ALPHR_VAULT } from "../constants/alphr";
 
 async function main() {
-  const feeStorage = await ethers
-    .getContractFactory('FeeStorage')
-    .then((deployFactory) =>
-      deployFactory.deploy(
-        KOVAN_ALPHR_TOKEN,
-        UNISWAP_ROUTER_V2,
-        KOVAN_ALPHR_VAULT
-      )
-    )
-    .then((market) => market.address);
-  console.log('FeeStorage contract deployed: %s', feeStorage);
+  // const feeStorage = await ethers
+  //   .getContractFactory('FeeStorage')
+  //   .then((deployFactory) =>
+  //     deployFactory.deploy(
+  //       KOVAN_ALPHR_TOKEN,
+  //       UNISWAP_ROUTER_V2,
+  //       KOVAN_ALPHR_VAULT
+  //     )
+  //   )
+  //   .then((market) => market.address);
+  // console.log('FeeStorage contract deployed: %s', feeStorage);
+  const feeStorage = "0x10b92D86873eb9EBD7AfEbeE398d39E41423628D";
 
   const feeQuota = 20;
   const feeQuotaDecimals = 10000;
   const manualTrade = await ethers
-    .getContractFactory('ManualTrade')
+    .getContractFactory("ManualTrade")
     .then((deployFactory) =>
       deployFactory.deploy(
         feeStorage,
@@ -30,7 +31,7 @@ async function main() {
       )
     )
     .then((asset) => asset.address);
-  console.log('ManualTrade contract deployed: %s', manualTrade);
+  console.log("ManualTrade contract deployed: %s", manualTrade);
 }
 
 main()
